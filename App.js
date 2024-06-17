0cb7de95d88349550e8bd95630dd30241e1bf964
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-06-17 22:40:48 +0200

    Fixing scroll bugs and adding add to cart functionality

--- App.js
+++ App.js
@@ -20,6 +20,13 @@ export default function App() {
     { 'name': 'Top', 'isSelected': false }
   ];
 
+function handleOrder(id) {
+  console.log(id)
+}
+
+function handleLike(id) {
+  console.log("liked")
+}
 
 
   if (!fontsLoaded && !fontError) {
@@ -36,30 +43,36 @@ export default function App() {
           <Image source={{uri:"https://i.ibb.co/dJmnyqp/xdxdxd.png", height: 11, width: 22}} />
         </View>
       <View style={styles.header}>
-        <Text style={{ fontSize: 20, color: '#27214D', width: '70%', fontFamily: 'Regular' }}>
+        <Text style={{ fontSize: 20, color: '#27214D', width: '80%', fontFamily: 'Regular' }}>
           Hello Maciej, <Text style={{ fontFamily: 'Bold' }}>What dish combo do you want today?</Text>
         </Text>
       </View>
-      <Text style={{ fontSize: 24, color: '#27214D', width: '70%', fontFamily: 'Medium', margin: 20 }}>
+      <Text style={{ fontSize: 24, color: '#27214D', width: '70%', fontFamily: 'Medium', marginVertical: 20, alignSelf:'flex-start', marginLeft:35}}>
         Recommended Combos
       </Text>
       <View style={styles.cardContainer}>
         <View style={styles.card}>
+        <Pressable onPress={() => handleLike(2)}>
           <Image source={{ uri: "https://i.ibb.co/VgHtxyy/Vector.png", height: 12, width: 14 }} style={{ justifyContent: 'flex-end', alignItems: 'flex-end', alignContent: 'flex-end', marginBottom: 5 }} />
+          </Pressable>
           <Image source={{ uri: "https://i.ibb.co/Sxvq9nx/Honey-Lime-Peach-Fruit-Salad-3-725x725-1-removebg-preview-1.png", height: 78, width: 78 }} style={{ marginBottom: 10 }} />
           <Text style={{ fontSize: 14, color: '#27214D', fontFamily: 'Medium', width: '100%' }}>Honey lime combo</Text>
           <View style={{ display: 'flex', flexDirection: 'row', paddingHorizontal: 11 }}>
             <Text style={{ fontSize: 12, color: '#F08626', fontFamily: 'Regular', width: '100%' }}>30 zł</Text>
+            <Pressable onPress={() => handleOrder(1)}>
             <Image source={{ uri: "https://i.ibb.co/gP7YmN7/Group-10.png", height: 22, width: 22 }} style={{ marginBottom: 10 }} />
+          </Pressable>
           </View>
         </View>
         <View style={styles.card}>
+        <Pressable onPress={() => handleLike(2)}>
           <Image source={{ uri: "https://i.ibb.co/VgHtxyy/Vector.png", height: 12, width: 14 }} style={{ justifyContent: 'flex-end', alignItems: 'flex-end', alignContent: 'flex-end', marginBottom: 5 }} />
+          </Pressable>
           <Image source={{ uri: "https://i.ibb.co/2F1VvD8/Glowing-Berry-Fruit-Salad-8-720x720-removebg-preview-1.png", height: 78, width: 78 }} style={{ marginBottom: 10 }} />
           <Text style={{ fontSize: 14, color: '#27214D', fontFamily: 'Medium', width: '100%' }}>Berry mango combo</Text>
           <View style={{ display: 'flex', flexDirection: 'row', paddingHorizontal: 11 }}>
             <Text style={{ fontSize: 12, color: '#F08626', fontFamily: 'Regular', width: '100%' }}>30 zł</Text>
-            <Pressable onPress={()=> console.log("kurwa")}>
+            <Pressable onPress={() => handleOrder(2)}>
             <Image source={{ uri: "https://i.ibb.co/gP7YmN7/Group-10.png", height: 22, width: 22 }} style={{ marginBottom: 10 }} />
             </Pressable>
           </View>
@@ -72,9 +85,11 @@ export default function App() {
       </View>
 
         
-      <ScrollView horizontal={true} style={styles.cardScrollContainer}> 
+      <ScrollView horizontal={true} style={{ minWidth: Dimensions.get('screen').width }}>
         <View style={styles.card1}>
+        <Pressable onPress={() => handleLike(3)}>
           <Image source={{ uri: "https://i.ibb.co/VgHtxyy/Vector.png", height: 12, width: 14 }} style={{ justifyContent: 'flex-end', alignItems: 'flex-end', alignContent: 'flex-end', marginBottom: 5 }} />
+        </Pressable>
           <Image source={{ uri: "https://i.ibb.co/D5py9RL/breakfast-quinoa-and-red-fruit-salad-134061-1-removebg-preview-1.png", height: 62, width: 62 }} style={{ marginBottom: 10 }} />
           <Text style={{ fontSize: 14, color: '#27214D', fontFamily: 'Medium', width: '100%' }}>Quinoa fruit salad</Text>
           <View style={{ display: 'flex', flexDirection: 'row', paddingHorizontal: 11 }}>
@@ -83,7 +98,9 @@ export default function App() {
           </View>
         </View>
         <View style={styles.card2}>
+        <Pressable onPress={() => handleLike(4)}>
           <Image source={{ uri: "https://i.ibb.co/VgHtxyy/Vector.png", height: 12, width: 14 }} style={{ justifyContent: 'flex-end', alignItems: 'flex-end', alignContent: 'flex-end', marginBottom: 5 }} />
+        </Pressable>  
           <Image source={{ uri: "https://i.ibb.co/FBGPwPF/Best-Ever-Tropical-Fruit-Salad8-WIDE-removebg-preview-1.png", height: 48, width: 94 }} style={{ marginBottom: 10 }} />
           <Text style={{ fontSize: 14, color: '#27214D', fontFamily: 'Medium', width: '100%' }}>Tropical fruit salad</Text>
           <View style={{ display: 'flex', flexDirection: 'row', paddingHorizontal: 11 }}>
@@ -92,7 +109,9 @@ export default function App() {
           </View>
         </View>
         <View style={styles.card3}>
+        <Pressable onPress={() => handleLike(5)}>
           <Image source={{ uri: "https://i.ibb.co/VgHtxyy/Vector.png", height: 12, width: 14 }} style={{ justifyContent: 'flex-end', alignItems: 'flex-end', alignContent: 'flex-end', marginBottom: 5 }} />
+          </Pressable>  
           <Image source={{ uri: "https://i.ibb.co/QNtKj4W/xddd.png", height: 42, width: 67 }} style={{ marginBottom: 10 }} />
           <Text style={{ fontSize: 14, color: '#27214D', fontFamily: 'Medium', width: '100%' }}>Tropical fruit salad</Text>
           <View style={{ display: 'flex', flexDirection: 'row', paddingHorizontal: 11 }}>
@@ -101,10 +120,6 @@ export default function App() {
           </View>
         </View>
       </ScrollView>
-
-
-
-
       </View>
 
   );
@@ -179,7 +194,7 @@ const styles = StyleSheet.create({
     height: '10%',
     width: '100%',
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: 30,
     },
     cardContainer: {
     display: 'flex',
